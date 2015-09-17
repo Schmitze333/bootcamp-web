8cf76a65326bce46d73c8fa5de37665a04cba41c
Marc Schmitz <marcschmitz333@gmail.com>
2015-09-16 19:13:56 -0500

    modify variables.rb

--- variables.rb
+++ variables.rb
@@ -1,16 +1,32 @@
+=begin
+	Defining some variables and print them out.
+	This explains the differences between puts and print.	
+=end
+
+# Definí una variable con un float
 var = 12.3
+# Definí una variable con texto
 var2 = "segundo variable"
+# Definí una variable boolean
 var3 = true
+# Cuento con variables
 sum = var + 34.9
+# Y, a nuevo una variable con texto
 text = "texto"
 
+# Imprimí todas datos con puts
 puts var
+puts "***************"
 puts text
+puts "***************"
 puts var2
 puts var3
 puts sum
 
+# Imprimí todas datos con print
 print var
 print text
 print var2
 print var3
+print sum
+puts
