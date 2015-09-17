4a662c67d5d2cb062ccb3ae724ff4dcbd4352ce1
Marc Schmitz <marcschmitz333@gmail.com>
2015-09-16 19:02:45 -0500

    updeate variables.rb

---
+++ variables.rb
@@ -0,0 +1,16 @@
+var = 12.3
+var2 = "segundo variable"
+var3 = true
+sum = var + 34.9
+text = "texto"
+
+puts var
+puts text
+puts var2
+puts var3
+puts sum
+
+print var
+print text
+print var2
+print var3
