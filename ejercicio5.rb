44e1f129849cb4d19f0222e743d4006d5a6b9ef2
Marc Schmitz <marcschmitz333@gmail.com>
2015-09-17 21:28:02 -0500

    Ejercicio para operadores

---
+++ ejercicio5.rb
@@ -0,0 +1,19 @@
+# Realiza la comparación de dos variables.
+# Si las variables NO son iguales deberías imprimir un mensaje de alerta.
+var1 = 3
+var2 = 5
+
+if var1 != var2
+	puts "Estas variables NO son iguales!"
+end
+
+# Realiza la comparación de cuatro valores, cada uno de ellos debería cumplirse.
+puts 3 == 3 && "f" == "f" && 8 == 8 && "valor" == "valor"
+
+# Realiza la comparación de dos números enteros, si son iguales muestra un mensaje de alerta.
+if var1 == var2
+	puts "Estas variables son iguales."
+end
+
+# Suma dos números enteros que como resultado den 5 y luego verifica si es el cinco es mayor o igual a 7.
+puts (3 + 2) >= 7 
