9defac63028234fe9aa21f426f1e5b2aa909eae7
Marc Schmitz <marcschmitz333@gmail.com>
2015-09-17 19:19:22 -0500

    Para explicar arreglos

---
+++ arreglo.rb
@@ -0,0 +1,13 @@
+SegundoArreglo = ["Carlos",
+			"Ramiro",
+			"Nohora",
+			"Diego",
+			"Daniel",
+			"Amanda",
+			"Marc"]
+
+# Use of each
+puts "********************"
+SegundoArreglo.each do |alumno|
+	puts alumno
+end
