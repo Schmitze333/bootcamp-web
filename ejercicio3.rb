3aa2b51b91af2095805e44467e55a725dac0e417
Marc Schmitz <marcschmitz333@gmail.com>
2015-09-17 19:44:55 -0500

    Ejercicio para identificar errors

---
+++ ejercicio3.rb
@@ -0,0 +1,13 @@
+Nombre = "Marc"
+nombre = "marc"
+
+EDAD = "36"
+cuidad = "Bogotá"
+
+if cuidad == "Bogotá" then
+	puts "La ciudad es Bogotá"
+end
+
+arreglo = ["Manazana", "Limon", 3, 2.4, "Pera"]
+
+puts arreglo[-1]
