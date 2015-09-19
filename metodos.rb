bc21c6e47a39cc53154957b17831324136bfbe9f
Marc Schmitz <marcschmitz333@gmail.com>
2015-09-18 19:31:29 -0500

    ejemplos por metodos

---
+++ metodos.rb
@@ -0,0 +1,17 @@
+def saludar
+	puts "Hola, bienvenido."
+	if "marc" == "marc"
+		puts "marc SÍ es igual a marc"
+	else
+		puts "marc NO es igual a marc"
+	end
+end
+
+saludar
+
+def hablar(texto)
+	puts "Elmétodo dice: #{texto}"
+end
+
+hablar("Estoy hablando con un método que recibe argumentos.")
+
